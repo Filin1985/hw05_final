8f24f55153080b0c8bdec4a3af24a3a87f634278
Marat Ikhsanov <m.ihsanov2009@yandex.ru>
2022-08-06 15:47:14 +0300

    Update views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -40,7 +40,8 @@ def profile(request, username):
             & Q(user=request.user)
         ):
             following = True
-    except:
+            raise Exception("Подписка не оформлена")
+    except Exception:
         following = False
     author = get_object_or_404(User, username=username)
     return render(request, 'posts/profile.html', {
